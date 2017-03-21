a2e01faea33ee13a0e1214dba6b6972c5c1a4cad
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-03-21 09:44:37 +0700

    Finished!! Decrypt File

--- rijndael.c
+++ rijndael.c
@@ -12,10 +12,6 @@ int fileMode(char *filepath, unsigned char *key, int is_encrypt) {
 
 	FILE *fp, *fn;
 	fp = fopen(filepath, "rb");
-	char *encFile = (char*)calloc(strlen(filepath)+11, sizeof(char));
-	strcpy(encFile, filepath);
-	strcat(encFile, ".encrypted");
-	fn = fopen(encFile, "wb");
 	if (fp == NULL) {
 		printf("File doesn't exist!!\n");
 		return 0;
@@ -25,18 +21,46 @@ int fileMode(char *filepath, unsigned char *key, int is_encrypt) {
 	fseek(fp, 0, SEEK_END);
 	int size = ftell(fp);
 	rewind(fp);
+
 	printf("File name: %s\n", filepath);
 	printf("File size: %d byte(s)\n", size);
+
+	//Decrypt
+	if (!is_encrypt) {
+		char *decFile = (char*)calloc(strlen(filepath), sizeof(char));
+		strcpy(decFile, filepath);
+		decFile[strlen(decFile)-10] = '\0'; //Delete .encrypted
+		printf("Decrypted file: %s\n", decFile);
+		fn = fopen(decFile, "wb");
+
+		for (int i = 0; i < size; i+=16) {
+			unsigned char *buff = (unsigned char*)calloc(BLOCK_SIZE, sizeof(unsigned char));
+			fread(buff, 1, BLOCK_SIZE, fp);
+			Decrypt(buff, expandedKey);
+			fwrite(buff, 1, BLOCK_SIZE, fn);
+		}
+
+		fclose(fn);
+		fclose(fp);
+		return 0;
+	}
+
+	//Encrypt
+	char *encFile = (char*)calloc(strlen(filepath)+11, sizeof(char));
+	strcpy(encFile, filepath);
+	strcat(encFile, ".encrypted");
+	fn = fopen(encFile, "wb");
 	printf("Encrypted file: %s\n", encFile);
 
 	//File doesn't need to be padded because calloc will auto padded with \0x00
-	int i;
-	for (i = 0; i < size; i+=16) {
+	for (int i = 0; i < size; i+=16) {
 		unsigned char *buff = (unsigned char*)calloc(BLOCK_SIZE, sizeof(unsigned char));
 		fread(buff, 1, BLOCK_SIZE, fp);
 		Encrypt(buff, expandedKey);
 		fwrite(buff, 1, BLOCK_SIZE, fn);
 	}
+	fclose(fn);
+	fclose(fp);
 	return 0;
 }
 
